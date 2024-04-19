5d0c582d61b7aefbfc6ae03528ecbd03b709addb
B. Haki Martin <jojackblack@gmail.com>
2024-04-19 19:52:29 -0400

    added argon2 for password hashing and salting. Implemented it in auth route.

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/UserModel");
 const router = express.Router();
+const argon2 = require("argon2");
 
 router.get("/", (req, res) => {
     res.send({msg: "got to auth"});
@@ -9,8 +10,17 @@ router.get("/", (req, res) => {
 router.post("/signup", async (req, res) => {
     console.log(req.body);
     const { username, email, password } = req.body;
+    
+    let hashedPass;
     try{
-        const newUser = await User.create({email: email, password: password, username: username, savedMessages: []});
+        hashedPass = await argon2.hash(password);
+        console.log(hashedPass);
+    }
+    catch(err){
+        console.log(err);
+    }
+    try{
+        const newUser = await User.create({email: email, password: hashedPass, username: username, savedMessages: []});
         res.send({user: newUser});
     }
     catch(err){
@@ -19,7 +29,31 @@ router.post("/signup", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-    // 
+    console.log(req.body);
+    const { email, password } = req.body;
+    const findUser = await User.findOne({email});
+
+    if(findUser){
+        console.log("found user");
+        try{
+            if(await argon2.verify(findUser.password, password)){
+                console.log("pass match");
+                res.send({"user": findUser});
+            }
+            else{
+                console.log("mismatch passwords");
+                res.send({"error": "passowrds dont match"});
+            }
+        }
+        catch(err){
+            console.log(err);
+            res.send({"err":""});
+        }
+    }
+    else{
+        console.log("found user");
+        res.send({"err": "No user found"});
+    }
 });
 
 module.exports = router;
