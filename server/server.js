e48b4e320b8dc10eaf381926f779aef6039bb3fb
B. Haki Martin <jojackblack@gmail.com>
2024-04-19 14:13:09 -0400

    finally sending user data to database

--- server/server.js
+++ server/server.js
@@ -6,10 +6,13 @@ const mongoose = require("mongoose");
 const mongooseURI = "mongodb+srv://jojackblack:IMN6Sfl5cesPKWL8@sc-cluster.bt1tdfm.mongodb.net/?retryWrites=true&w=majority&appName=SC-Cluster";
 const authController = require("./controllers/AuthController.js");
 
-app.use("/auth", authController);
+//MARK: Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+//MARK: Routes
+app.use("/auth", authController);
 
 mongoose.connect(mongooseURI).then( () => {
     app.listen(port, ()=>{
