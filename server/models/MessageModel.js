49f2c5725ae676f083a7e158d9111304c7e879fe
B. Haki Martin <jojackblack@gmail.com>
2024-04-19 01:34:07 -0400

    deleted database. Modified server to connect to mongoose then start listening for requests.

--- server/models/MessageModel.js
+++ server/models/MessageModel.js
@@ -1,7 +1,9 @@
-const { model, Schema } = require("mongoose");
+const { model, Schema, default: mongoose } = require("mongoose");
 
 const messageSchema = new Schema({
-    content: String
+    content: String,
+    keywords: [String],
+    user: {type: mongoose.Types.ObjectId, ref: "User"}
 })
 
 const Message = model("Message", messageSchema);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+const mongooseURI = "mongodb+srv://jojackblack:IMN6Sfl5cesPKWL8@sc-cluster.bt1tdfm.mongodb.net/?retryWrites=true&w=majority&appName=SC-Cluster";
+
 
 app.get("/", (req, res) => {
     console.log(req.body);
-    res.send("hello");
+    res.send({msg: "hello tst"});
 })
 
-app.listen(port, ()=>{
-    console.log(`Listening on port ${port}`);
+mongoose.connect(mongooseURI).then( () => {
+    app.listen(port, ()=>{
+        console.log(`Listening on port ${port}`);
+    })
 })
 
 // mongodb+srv://jojackblack:IMN6Sfl5cesPKWL8@sc-cluster.bt1tdfm.mongodb.net/?retryWrites=true&w=majority&appName=SC-Cluster
