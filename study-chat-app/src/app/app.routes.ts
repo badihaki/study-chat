baad71e8f2aaffeea74f2655820684c4621a3f7b
B. Haki Martin <jojackblack@gmail.com>
2024-04-24 21:11:11 -0400

    css stuff for nav bar. made an error page component.

--- study-chat-app/src/app/app.routes.ts
+++ study-chat-app/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { LandingComponent } from './landing/landing.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: LandingComponent
     },
+    {
+        path: "**",
+        component: ErrorComponent
+    }
 ];
