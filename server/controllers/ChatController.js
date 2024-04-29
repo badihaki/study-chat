2f2ba6e01a4a67a9bbab04e7cc339fb810446967
B. Haki Martin <jojackblack@gmail.com>
2024-04-29 15:13:58 -0400

    socket is set up to recieve messages. Ignore frontend

--- server/controllers/ChatController.js
+++ server/controllers/ChatController.js
@@ -1,13 +1,16 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
+const http = require("http").Server(router);
+const io = require("socket.io")(http);
 
-const { createServer } = require("node:http");
-const server = createServer(router);
-const { Server } = require("socket.io");
-const io = new Server(server,
-{
-    path: "../server.js"
-});
+// const express = require("express");
+// const router = express.Router();
+// const { createServer } = require("node:http");
+// const server = createServer(router);
+// const { Server } = require("socket.io");
+// const io = new Server(server,
+// {
+//     path: "../server.js"
+// });
 
 router.get("", ( req, res ) => {
     console.log(req);
@@ -16,6 +19,19 @@ router.get("", ( req, res ) => {
 
 io.on('connection', (socket) => {
     console.log("someone connected");
+    let previousID;
+
+    const safeJoin = ( currentID ) => {
+        socket.leave(previousID);
+        socket.join(currentID, () => {
+            console.log(`Socket ${socket.id} joined room ${currentID}`);
+        })
+        previousID = currentID;
+    }
+
+    socket.on("getChat", (msg) => {
+        console.log(`Message recieved: ${msg}`);
+    })
 });
 
 module.exports = router;
