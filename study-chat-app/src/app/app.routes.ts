77911d150dc2039b2eb361c23503c91a960f5ac2
B. Haki Martin <jojackblack@gmail.com>
2024-04-28 20:24:30 -0400

    created a chatroom view, updated routing

--- study-chat-app/src/app/app.routes.ts
+++ study-chat-app/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { LandingComponent } from './landing/landing.component';
 import { ErrorComponent } from './error/error.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: LandingComponent
+    },
+    {
+        path: "chat",
+        component: ChatRoomComponent
     },
     {
         path: "**",
