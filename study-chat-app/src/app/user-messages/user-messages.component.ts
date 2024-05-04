897d3fc40341a1d69bba711b4ac2232947f1d321
B. Haki Martin <jojackblack@gmail.com>
2024-05-04 12:42:03 -0400

    getting messages in the front end. For the backend: had to use for..of loop to run async ops. Also set up routes to work with :id param;; For frontend: set up http client

--- study-chat-app/src/app/user-messages/user-messages.component.ts
+++ study-chat-app/src/app/user-messages/user-messages.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { UserService } from '../_services/user.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-messages',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './user-messages.component.html',
   styleUrl: './user-messages.component.scss'
 })
-export class UserMessagesComponent {
+export class UserMessagesComponent implements OnInit {
+  userService:UserService = inject(UserService);
+  http:HttpClient = inject(HttpClient);
+  serverAddress:string = "http://localhost:3000/messages";
 
+  ngOnInit(): void {
+    // console.log(this.userService.user?._id);
+    this.http.post(`${this.serverAddress}/${this.userService.user?._id}`, this.userService.user?.savedMessages).subscribe( (res) => {
+      console.log(res);
+    })
+  }
+
+  getUseInfor(){
+    console.log(this.userService.user);
+  }
 }
